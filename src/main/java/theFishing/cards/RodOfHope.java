d65bc9a544781d71ecba4288d40968a974a65564
DarkVexon <edanvirgo@gmail.com>
2022-08-04 10:05:03 -0700

    Bug fixes

--- src/main/java/theFishing/cards/RodOfHope.java
+++ src/main/java/theFishing/cards/RodOfHope.java
@@ -1,28 +1,17 @@
 package theFishing.cards;
 
-import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
-import com.megacrit.cardcrawl.actions.watcher.SkipEnemiesTurnAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.BlurPower;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
-import com.megacrit.cardcrawl.vfx.combat.WhirlwindEffect;
-import theFishing.FishingMod;
-import theFishing.powers.DrawLessNextTurnPower;
-import theFishing.powers.LambdaPower;
-
 
 import static theFishing.FishingMod.makeID;
-import static theFishing.util.Wiz.applyToSelf;
 import static theFishing.util.Wiz.atb;
+import static theFishing.util.Wiz.att;
 
 public class RodOfHope extends AbstractFishingCard {
     public final static String ID = makeID("RodOfHope");
@@ -31,21 +20,31 @@ public class RodOfHope extends AbstractFishingCard {
     public RodOfHope() {
         super(ID, 1, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
 
-        baseDamage = 2;
+        baseDamage = 5;
         baseMagicNumber = magicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
         if (isVoyaged()) {
-            atb(new GainEnergyAction(Math.max(p.energy.energyMaster - EnergyPanel.totalCount, 0)));
+            atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    isDone = true;
+                    att(new GainEnergyAction(Math.max(AbstractDungeon.player.energy.energyMaster - EnergyPanel.totalCount, 0)));
+                }
+            });
             atb(new DrawCardAction(magicNumber));
             exhaust = true;
         }
     }
 
+    public void triggerOnGlowCheck() {
+        this.glowColor = isVoyaged() ? AbstractCard.GOLD_BORDER_GLOW_COLOR : AbstractCard.BLUE_BORDER_GLOW_COLOR;
+    }
+
     public void upp() {
-        upgradeMagicNumber(-1);
-        uDesc();
+        upgradeDamage(1);
+        upgradeMagicNumber(1);
     }
 }
