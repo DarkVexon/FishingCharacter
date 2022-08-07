d68fa0305c3c33335adb56a8175eee4675d46c8a
DarkVexon <edanvirgo@gmail.com>
2022-08-06 23:51:27 -0700

    Glitter Glue replaces Pick Me

--- src/main/java/theFishing/cards/GlitterGlue.java
+++ src/main/java/theFishing/cards/GlitterGlue.java
@@ -6,29 +6,34 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
 import theFishing.patch.foil.FoilPatches;
 
 import java.util.ArrayList;
 
-
 import static theFishing.FishingMod.makeID;
+import static theFishing.util.Wiz.applyToEnemy;
 
-public class PickMe extends AbstractFishingCard implements SpawnModificationCard {
-    public final static String ID = makeID("PickMe");
-    // intellij stuff attack, all, uncommon, 9, 2, 3, 1, , 
+public class GlitterGlue extends AbstractFishingCard implements SpawnModificationCard {
+    public final static String ID = makeID("GlitterGlue");
+    // intellij stuff skill, enemy, uncommon, , , , , , 
 
-    public PickMe() {
-        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL);
-        baseDamage = 9;
-        baseBlock = 3;
-        isMultiDamage = true;
-
+    public GlitterGlue() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = 6;
+        baseMagicNumber = magicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        blck();
-        allDmg(AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        applyToEnemy(m, new WeakPower(m, magicNumber, false));
     }
+
+    public void upp() {
+        upgradeDamage(1);
+        upgradeMagicNumber(1);
+    }
+
 
     @Override
     public void onRewardListCreated(ArrayList<AbstractCard> rewardCards) {
@@ -44,8 +49,4 @@ public class PickMe extends AbstractFishingCard implements SpawnModificationCard
         }
     }
 
-    public void upp() {
-        upgradeDamage(1);
-        upgradeBlock(2);
-    }
 }
