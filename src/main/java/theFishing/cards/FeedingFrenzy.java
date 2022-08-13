61efa441c6262851f6cec456f5c35329c290cb59
DarkVexon <edanvirgo@gmail.com>
2022-08-13 11:33:54 -0700

    Goodbye, Supernova

--- src/main/java/theFishing/cards/FeedingFrenzy.java
+++ src/main/java/theFishing/cards/FeedingFrenzy.java
@@ -2,9 +2,11 @@ package theFishing.cards;
 
 import basemod.ReflectionHacks;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -14,8 +16,10 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import com.megacrit.cardcrawl.vfx.combat.BiteEffect;
+import com.megacrit.cardcrawl.vfx.combat.ShockWaveEffect;
 import com.megacrit.cardcrawl.vfx.combat.ThrowDaggerEffect;
 import theFishing.cards.fish.AbstractFishCard;
+import theFishing.patch.foil.FoilPatches;
 
 import static theFishing.FishingMod.makeID;
 import static theFishing.util.Wiz.atb;
@@ -30,40 +34,58 @@ public class FeedingFrenzy extends AbstractFishingCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int count = 0;
-        for (AbstractCard c : p.drawPile.group) {
-            if (c instanceof AbstractFishCard) {
-                count++;
-            }
-        }
-        for (AbstractCard c : p.hand.group) {
-            if (c instanceof AbstractFishCard) {
-                count++;
-            }
-        }
-        for (AbstractCard c : p.discardPile.group) {
-            if (c instanceof AbstractFishCard) {
-                count++;
-            }
-        }
-        for (int i = 0; i < count; i++)
-            atb(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    isDone = true;
-                    AbstractMonster q = AbstractDungeon.getMonsters().getRandomMonster((AbstractMonster) null, true, AbstractDungeon.cardRandomRng);
-                    att(new DamageAction(q, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.NONE));
-                    att(new VFXAction(new BiteEffect(q.hb.cX, q.hb.cY)));
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                int x = 0;
+                for (AbstractCard q : AbstractDungeon.actionManager.cardsPlayedThisTurn) {
+                    if (q instanceof AbstractFishCard)
+                        x += 1;
                 }
-            });
+                if (x > 0)
+                    for (int i = 0; i < x; i++) {
+                        att(new AbstractGameAction() {
+                            @Override
+                            public void update() {
+                                isDone = true;
+                                AbstractMonster q = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+                                att(new DamageAction(q, new DamageInfo(p, damage, damageTypeForTurn), AttackEffect.NONE));
+                                att(new VFXAction(new BiteEffect(q.hb.cX, q.hb.cY)));
+                            }
+                        });
+                    }
+            }
+        });
+        this.rawDescription = cardStrings.DESCRIPTION;
+        this.initializeDescription();
     }
 
     @Override
     public void applyPowers() {
         super.applyPowers();
-        rawDescription = cardStrings.DESCRIPTION;
-        rawDescription += cardStrings.EXTENDED_DESCRIPTION[0];
-        initializeDescription();
+        int count = 0;
+        for (AbstractCard q : AbstractDungeon.actionManager.cardsPlayedThisTurn) {
+            if (q instanceof AbstractFishCard) {
+                count += 1;
+            }
+        }
+
+        this.rawDescription = cardStrings.DESCRIPTION;
+        this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[0] + count;
+        if (count == 1) {
+            this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[1];
+        } else {
+            this.rawDescription = this.rawDescription + cardStrings.EXTENDED_DESCRIPTION[2];
+        }
+
+        this.initializeDescription();
+    }
+
+    @Override
+    public void onMoveToDiscard() {
+        this.rawDescription = cardStrings.DESCRIPTION;
+        this.initializeDescription();
     }
 
     public void upp() {
