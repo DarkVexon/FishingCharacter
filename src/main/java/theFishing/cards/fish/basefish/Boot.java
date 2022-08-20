fefae1193a7c6c48731df1c054158511edbb2969
Vex Darkon <edanvirgo@gmail.com>
2022-08-20 10:29:29 -0700

    Boots become AOE

--- src/main/java/theFishing/cards/fish/basefish/Boot.java
+++ src/main/java/theFishing/cards/fish/basefish/Boot.java
@@ -2,7 +2,6 @@ package theFishing.cards.fish.basefish;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theFishing.cards.fish.AbstractFishCard;
@@ -15,12 +14,13 @@ public class Boot extends AbstractFishCard {
     // intellij stuff skill, none, special, , , , , 1, 1
 
     public Boot() {
-        super(ID, CardType.ATTACK, CardTarget.ENEMY);
+        super(ID, CardType.ATTACK, CardTarget.ALL_ENEMY);
         baseDamage = 1;
+        isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        allDmg(AbstractGameAction.AttackEffect.BLUNT_LIGHT);
         atb(new DrawCardAction(1));
     }
 
