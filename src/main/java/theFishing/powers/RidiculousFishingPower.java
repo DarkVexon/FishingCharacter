4d6e0baa100f2a40f95559616384a32ccac19a1b
boardengineer <77598527+boardengineer@users.noreply.github.com>
2022-09-10 01:38:06 -0700

    un-inline RidiculousFishing power

---
+++ src/main/java/theFishing/powers/RidiculousFishingPower.java
@@ -0,0 +1,34 @@
+package theFishing.powers;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static theFishing.FishingMod.makeID;
+
+public class RidiculousFishingPower extends LambdaPower {
+    public static String ID = makeID(RidiculousFishingPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(ID);
+
+    public RidiculousFishingPower(int amount) {
+        super(ID, powerStrings.NAME, AbstractPower.PowerType.BUFF, false, AbstractDungeon.player, amount);
+    }
+
+    @Override
+    public void onAfterCardPlayed(AbstractCard card) {
+        if (card.color == AbstractCard.CardColor.COLORLESS) {
+            flash();
+            addToBot(new DamageRandomEnemyAction(new DamageInfo(owner, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = powerStrings.DESCRIPTIONS[0] + amount + powerStrings.DESCRIPTIONS[1];
+    }
+}
