8174e86dba52daaa8e7da9f9700ea5091f04d07b
boardengineer <77598527+boardengineer@users.noreply.github.com>
2022-09-10 12:58:38 -0700

    Un-inline Fully Sleeved Power

--- src/main/java/theFishing/cards/FullySleeved.java
+++ src/main/java/theFishing/cards/FullySleeved.java
@@ -1,16 +1,11 @@
 package theFishing.cards;
 
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import theFishing.powers.LambdaPower;
+import theFishing.powers.FullySleevedPower;
 
 import static theFishing.FishingMod.makeID;
-import static theFishing.patch.foil.FoilPatches.isFoil;
 import static theFishing.util.Wiz.applyToSelf;
-import static theFishing.util.Wiz.atb;
 
 public class FullySleeved extends AbstractFishingCard {
     public final static String ID = makeID("FullySleeved");
@@ -22,20 +17,7 @@ public class FullySleeved extends AbstractFishingCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        applyToSelf(new LambdaPower(makeID("FullySleevedPower"), cardStrings.EXTENDED_DESCRIPTION[0], AbstractPower.PowerType.BUFF, false, p, magicNumber) {
-            @Override
-            public void atEndOfTurnPreEndTurnCards(boolean isPlayer) {
-                if (AbstractDungeon.actionManager.cardsPlayedThisTurn.stream().filter(q -> isFoil(q)).count() >= 2) {
-                    flash();
-                    atb(new GainBlockAction(owner, amount));
-                }
-            }
-
-            @Override
-            public void updateDescription() {
-                description = cardStrings.EXTENDED_DESCRIPTION[1] + amount + cardStrings.EXTENDED_DESCRIPTION[2];
-            }
-        });
+        applyToSelf(new FullySleevedPower(magicNumber));
     }
 
     public void upp() {
---
+++ src/main/java/theFishing/powers/FullySleevedPower.java
@@ -0,0 +1,33 @@
+package theFishing.powers;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static theFishing.FishingMod.makeID;
+import static theFishing.patch.foil.FoilPatches.isFoil;
+import static theFishing.util.Wiz.atb;
+
+public class FullySleevedPower extends LambdaPower {
+    public static String ID = makeID(FullySleevedPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(ID);
+
+    public FullySleevedPower(int amount) {
+        super(makeID("FullySleevedPower"), powerStrings.NAME, AbstractPower.PowerType.BUFF, false, AbstractDungeon.player, amount);
+    }
+
+    @Override
+    public void atEndOfTurnPreEndTurnCards(boolean isPlayer) {
+        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.stream().filter(q -> isFoil(q)).count() >= 2) {
+            flash();
+            atb(new GainBlockAction(owner, amount));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = powerStrings.DESCRIPTIONS[0] + amount + powerStrings.DESCRIPTIONS[1];
+    }
+}
