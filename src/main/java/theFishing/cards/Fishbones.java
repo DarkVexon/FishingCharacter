07b1dbabd6e25da8d81f9aa832a369fe0e12df64
Vex Darkon <edanvirgo@gmail.com>
2022-09-11 23:34:17 -0700

    Fishbones buff

--- src/main/java/theFishing/cards/Fishbones.java
+++ src/main/java/theFishing/cards/Fishbones.java
@@ -16,8 +16,8 @@ public class Fishbones extends AbstractFishingCard {
 
     public Fishbones() {
         super(ID, 2, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = 18;
-        baseMagicNumber = magicNumber = 1;
+        baseDamage = 16;
+        baseMagicNumber = magicNumber = 2;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
