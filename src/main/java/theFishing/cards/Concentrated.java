26423438925cc6ad373a58eaae4a5478e0176276
Vex Darkon <edanvirgo@gmail.com>
2023-07-29 17:08:15 -0400

    More text changes

--- src/main/java/theFishing/cards/Concentrated.java
+++ src/main/java/theFishing/cards/Concentrated.java
@@ -1,12 +1,15 @@
 package theFishing.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.NewQueueCardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.BerserkPower;
 import theFishing.patch.UnnateCoffeePatch;
 
 import static theFishing.FishingMod.makeID;
-import static theFishing.util.Wiz.applyToSelf;
+import static theFishing.util.Wiz.*;
 
 public class Concentrated extends AbstractFishingCard {
     public final static String ID = makeID("Concentrated");
@@ -24,5 +27,17 @@ public class Concentrated extends AbstractFishingCard {
     public void upp() {
         UnnateCoffeePatch.UnnateField.unnate.set(this, false);
         uDesc();
+    }
+
+    @Override
+    public void triggerOnExhaust() {
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                AbstractDungeon.player.exhaustPile.removeCard(Concentrated.this);
+                att(new NewQueueCardAction(Concentrated.this, true, false, true));
+            }
+        });
     }
 }
