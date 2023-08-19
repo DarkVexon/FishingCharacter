6f71b1cca324a173f5d77cdb5e974400468bf4aa
Vex <edanvirgo@gmail.com>
2023-08-19 12:23:01 -0700

    Foil more frequent

--- src/main/java/theFishing/patch/foil/FoilPatches.java
+++ src/main/java/theFishing/patch/foil/FoilPatches.java
@@ -32,15 +32,11 @@ public class FoilPatches {
 
     // GAMEPLAY STUFF
 
-    public static final int FOIL_RARITY = 5; // 1 out of X card rewards
+    public static final int FOIL_RARITY = 4; // 1 out of X card rewards
 
-    public static final int SHOP_FOIL_CARDS = 2; // X out of the shop's cards
     public static final float SHOP_FOIL_MARKUP = 1.1F; // how much more expensive foils should be
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = SpirePatch.CLASS
-    )
+    @SpirePatch(clz = AbstractCard.class, method = SpirePatch.CLASS)
     public static class FoilField {
         public static SpireField<Boolean> foil = new SpireField<>(() -> false);
     }
@@ -62,10 +58,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = "makeStatEquivalentCopy"
-    )
+    @SpirePatch(clz = AbstractCard.class, method = "makeStatEquivalentCopy")
     public static class FoilCopies {
         public static AbstractCard Postfix(AbstractCard __result, AbstractCard __instance) {
             if (isFoil(__instance)) {
@@ -76,10 +69,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch2(
-            clz = AbstractDungeon.class,
-            method = "getRewardCards"
-    )
+    @SpirePatch2(clz = AbstractDungeon.class, method = "getRewardCards")
     public static class FoilInRewards {
         public static void Postfix(ArrayList<AbstractCard> __result) {
             if (AbstractDungeon.player != null && AbstractDungeon.player.chosenClass == TheFishing.Enums.THE_FISHING) {
@@ -89,50 +79,40 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = ShopScreen.class,
-            method = "initCards"
-    )
+    @SpirePatch(clz = ShopScreen.class, method = "initCards")
     public static class FoilInShops {
         public static void Postfix(ShopScreen __instance) {
             if (AbstractDungeon.player != null && AbstractDungeon.player.chosenClass == TheFishing.Enums.THE_FISHING) {
-                ArrayList<AbstractCard> shopCards = new ArrayList<>();
                 ArrayList<AbstractCard> coloredCards = ReflectionHacks.getPrivate(__instance, ShopScreen.class, "coloredCards");
                 ArrayList<AbstractCard> colorlessCards = ReflectionHacks.getPrivate(__instance, ShopScreen.class, "colorlessCards");
-                shopCards.addAll(coloredCards);
-                shopCards.addAll(colorlessCards);
                 if (AbstractDungeon.player.hasRelic(Newsletter.ID)) {
+                    ArrayList<AbstractCard> shopCards = new ArrayList<>();
+                    shopCards.addAll(coloredCards);
+                    shopCards.addAll(colorlessCards);
                     for (AbstractCard q : shopCards) {
                         makeFoil(q);
-                        if (q.canUpgrade())
-                            q.upgrade();
+                        if (q.canUpgrade()) q.upgrade();
                         q.price *= Newsletter.SHOP_CARD_PRICE_REDUCE;
                     }
                 } else {
-                    for (int i = 0; i < SHOP_FOIL_CARDS; i++) {
-                        AbstractCard target = shopCards.remove(AbstractDungeon.cardRng.random(shopCards.size() - 1));
-                        makeFoil(target);
-                        target.price *= SHOP_FOIL_MARKUP;
-                    }
+                    AbstractCard target1 = coloredCards.remove(AbstractDungeon.cardRng.random(coloredCards.size() - 1));
+                    makeFoil(target1);
+                    target1.price *= SHOP_FOIL_MARKUP;
+                    AbstractCard target2 = colorlessCards.remove(AbstractDungeon.cardRng.random(colorlessCards.size() - 1));
+                    makeFoil(target2);
+                    target2.price *= SHOP_FOIL_MARKUP;
                 }
             }
         }
     }
 
-    @SpirePatch(
-            clz = ShopScreen.class,
-            method = "purchaseCard"
-    )
+    @SpirePatch(clz = ShopScreen.class, method = "purchaseCard")
     public static class RestockWithNewsletter {
-        @SpireInsertPatch(
-                locator = Locator.class,
-                localvars = {"c"}
-        )
+        @SpireInsertPatch(locator = Locator.class, localvars = {"c"})
         public static void Insert(ShopScreen __instance, AbstractCard hoveredCard, AbstractCard c) {
             if (AbstractDungeon.player.hasRelic(Newsletter.ID)) {
                 makeFoil(c);
-                if (c.canUpgrade())
-                    c.upgrade();
+                if (c.canUpgrade()) c.upgrade();
                 c.price *= Newsletter.SHOP_CARD_PRICE_REDUCE;
             }
         }
@@ -146,10 +126,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = NeowReward.class,
-            method = "getColorlessRewardCards"
-    )
+    @SpirePatch(clz = NeowReward.class, method = "getColorlessRewardCards")
     public static class NeowFoils1 {
         public static ArrayList<AbstractCard> Postfix(ArrayList<AbstractCard> __result, NeowReward __instance, boolean rareOnly) {
             if (AbstractDungeon.player.chosenClass.equals(TheFishing.Enums.THE_FISHING)) {
@@ -160,10 +137,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = NeowReward.class,
-            method = "getRewardCards"
-    )
+    @SpirePatch(clz = NeowReward.class, method = "getRewardCards")
     public static class NeowFoils2 {
         public static ArrayList<AbstractCard> Postfix(ArrayList<AbstractCard> __result, NeowReward __instance, boolean rareOnly) {
             if (AbstractDungeon.player.chosenClass.equals(TheFishing.Enums.THE_FISHING)) {
@@ -203,10 +177,7 @@ public class FoilPatches {
 
     public static final ShaderProgram ART_SHADER = new ShaderProgram(SpriteBatch.createDefaultShader().getVertexShaderSource(), Gdx.files.internal("fishingResources/shaders/foil_card_art.frag").readString(String.valueOf(StandardCharsets.UTF_8)));
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = "renderPortrait"
-    )
+    @SpirePatch(clz = AbstractCard.class, method = "renderPortrait")
     public static class FoilSpecialArt {
         private static ShaderProgram oldShader;
         private static Color oldColor;
@@ -258,10 +229,7 @@ public class FoilPatches {
 //        }
 //    }
 
-    @SpirePatch(
-            clz = SingleCardViewPopup.class,
-            method = "renderPortrait"
-    )
+    @SpirePatch(clz = SingleCardViewPopup.class, method = "renderPortrait")
     public static class FoilSpecialArtSingleCardView {
         private static ShaderProgram oldShader;
         private static Color oldColor;
@@ -318,10 +286,7 @@ public class FoilPatches {
         sb.draw(img, drawX + img.offsetX - (float) img.originalWidth / 2.0F, drawY + img.offsetY - (float) img.originalHeight / 2.0F, (float) img.originalWidth / 2.0F - img.offsetX, (float) img.originalHeight / 2.0F - img.offsetY, (float) img.packedWidth, (float) img.packedHeight, drawScale * Settings.scale, drawScale * Settings.scale, angle);
     }
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = "renderAttackBg"
-    )
+    @SpirePatch(clz = AbstractCard.class, method = "renderAttackBg")
     public static class FoilColorlessLooksCool1 {
         private static final TextureAtlas.AtlasRegion colorlessFoilAttackAtlasRegion = ImageHelper.asAtlasRegion(TexLoader.getTexture(makeImagePath("512/colorless_attack_foil.png")));
 
@@ -335,10 +300,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = "renderSkillBg"
-    )
+    @SpirePatch(clz = AbstractCard.class, method = "renderSkillBg")
     public static class FoilColorlessLooksCool2 {
         private static final TextureAtlas.AtlasRegion colorlessFoilSkillAtlasRegion = ImageHelper.asAtlasRegion(TexLoader.getTexture(makeImagePath("512/colorless_skill_foil.png")));
 
@@ -352,10 +314,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = AbstractCard.class,
-            method = "renderPowerBg"
-    )
+    @SpirePatch(clz = AbstractCard.class, method = "renderPowerBg")
     public static class FoilColorlessLooksCool3 {
         private static final TextureAtlas.AtlasRegion colorlessFoilPowerAtlasRegion = ImageHelper.asAtlasRegion(TexLoader.getTexture(makeImagePath("512/colorless_power_foil.png")));
 
@@ -369,10 +328,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = SingleCardViewPopup.class,
-            method = "getCardBackAtlasRegion"
-    )
+    @SpirePatch(clz = SingleCardViewPopup.class, method = "getCardBackAtlasRegion")
     public static class FoilColorlessLooksCool4 {
         private static final TextureAtlas.AtlasRegion colorlessFoilAttackAtlasRegion1024 = ImageHelper.asAtlasRegion(TexLoader.getTexture(makeImagePath("1024/colorless_attack_foil.png")));
         private static final TextureAtlas.AtlasRegion colorlessFoilSkillAtlasRegion1024 = ImageHelper.asAtlasRegion(TexLoader.getTexture(makeImagePath("1024/colorless_skill_foil.png")));
@@ -396,15 +352,7 @@ public class FoilPatches {
         }
     }
 
-    @SpirePatch(
-            clz = AbstractDungeon.class,
-            method = "transformCard",
-            paramtypez = {
-                    AbstractCard.class,
-                    boolean.class,
-                    Random.class
-            }
-    )
+    @SpirePatch(clz = AbstractDungeon.class, method = "transformCard", paramtypez = {AbstractCard.class, boolean.class, Random.class})
     public static class TransformFoilsToFoils {
         public static void Postfix(AbstractCard c, boolean autoUpgrade, Random rng) {
             if (isFoil(c)) {
