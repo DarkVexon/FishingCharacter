e2f48f0b4f2fce829d3915c8996c94796f883fc6
DarkVexon <edanvirgo@gmail.com>
2024-04-11 18:49:38 -0700

    Up to date

--- src/main/java/theFishing/potions/StarlightSoda.java
+++ src/main/java/theFishing/potions/StarlightSoda.java
@@ -1,17 +1,13 @@
 package theFishing.potions;
 
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.PotionStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.powers.FrailPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 import theFishing.FishingMod;
 
 import static theFishing.FishingMod.makeID;
@@ -38,10 +34,8 @@ public class StarlightSoda extends AbstractPotion {
 
     @Override
     public void use(AbstractCreature abstractCreature) {
-        atb(new RemoveSpecificPowerAction(AbstractDungeon.player, AbstractDungeon.player, WeakPower.POWER_ID));
-        atb(new RemoveSpecificPowerAction(AbstractDungeon.player, AbstractDungeon.player, VulnerablePower.POWER_ID));
-        atb(new RemoveSpecificPowerAction(AbstractDungeon.player, AbstractDungeon.player, FrailPower.POWER_ID));
-        atb(new GainBlockAction(AbstractDungeon.player, potency));
+        atb(new RemoveDebuffsAction(AbstractDungeon.player));
+        atb(new HealAction(AbstractDungeon.player, AbstractDungeon.player, potency));
     }
 
     @Override
