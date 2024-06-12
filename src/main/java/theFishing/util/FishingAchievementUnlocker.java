6e2121420107b1e314ca38ef27fb9f8d6b1f44a5
Cany0udance <blakkrum@aol.com>
2024-06-11 22:33:25 -0400

    Removed publisher integration

--- src/main/java/theFishing/util/FishingAchievementUnlocker.java
+++ src/main/java/theFishing/util/FishingAchievementUnlocker.java
@@ -8,7 +8,6 @@ import static com.megacrit.cardcrawl.unlock.UnlockTracker.achievementPref;
 public class FishingAchievementUnlocker {
     public static void unlockAchievement(String key) {
         if (!Settings.isShowBuild && Settings.isStandardRun()) {
-            CardCrawlGame.publisherIntegration.unlockAchievement(key);
             if (!achievementPref.getBoolean(key, false)) {
                 achievementPref.putBoolean(key, true);
             }
